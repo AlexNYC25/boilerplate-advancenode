daa415821bcec3a44e868dded0c8b6bf6f321a21
Alexis Montes <montes.alexis25@gmail.com>
2021-07-27 23:06:16 -0400

    imported pug for express

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const pug = require('pug');
 
 const app = express();
 
@@ -10,6 +11,7 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
 
 app.route('/').get((req, res) => {
   res.render('Load your view here');
